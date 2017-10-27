a26f168d376a4658d126f079f92bbd523595a53a
avondo yann <yann.avondo@gmail.com>
2017-10-27 16:18:38 +0200

    :sparkles: Connexion screen WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ProfilPage } from '../pages/profil/profil';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ConnexionPage } from '../pages/connexion/connexion'
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,9 +16,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ProfilPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ConnexionPage
   ],
   imports: [
     BrowserModule,
@@ -27,9 +29,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ProfilPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ConnexionPage
   ],
   providers: [
     StatusBar,
--- src/pages/connexion/connexion.ts
+++ src/pages/connexion/connexion.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-connexion',
+  templateUrl: 'Connexion.html'
 })
-export class ContactPage {
+export class ConnexionPage {
 
   constructor(public navCtrl: NavController) {
 
