368d423545b2dc023f5757dc5e3fc9c579a719cb
avondo yann <yann.avondo@gmail.com>
2017-10-29 16:28:33 +0100

    :sparkles: Toast on login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,19 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
 import { SignupPage } from '../signup/signup';
-import { TabsPage} from '../tabs/tabs'
+import { TabsPage } from '../tabs/tabs';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
   }
-    login(){
-    this.navCtrl.push(TabsPage);
-    }
-    signup(){
+  Login() {
+    this.presentToast();
+  }
+  Signup() {
     this.navCtrl.push(SignupPage);
-    }
+  }
+  presentToast() {
+    const toast = this.toastCtrl.create({
+      message: 'Login Sucessful',
+      duration: 1000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      this.navCtrl.push(TabsPage);
+    });
+    toast.present();
+  }
 }
