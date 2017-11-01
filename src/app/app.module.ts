6e662ddff2af2f3da8df012bf02f6657e7de4151
avondo yann <yann.avondo@gmail.com>
2017-11-01 21:52:33 +0100

    :construction: Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { TransferPage } from '../pages/transfer/transfer';
 import { ProfilPage } from '../pages/profil/profil';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+//import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    TransferPage,
     ProfilPage,
     HomePage,
     TabsPage,
-    LoginPage
+    LoginPage,
     SignupPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FormsModule,
+    HttpClientModule
+   // HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    TransferPage,
     ProfilPage,
     HomePage,
     TabsPage,
-    LoginPage
+    LoginPage,
     SignupPage
   ],
   providers: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,25 +4,41 @@ import { ToastController } from 'ionic-angular';
 
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
+//import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http'; 
+
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
+  private results;
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+      public toastCtrl: ToastController,
+      private user:HttpClient) {
+  
+  }
+  logForm(form) {    
+    this.user.put('http://localhost:3636/auth/login',form.value)
+    .subscribe(
+      data => {
+          this.Login();
+      }
+    );
+    
   }
   Login() {
-    this.presentToast();
+    this.presentToast("lol");
   }
   Signup() {
     this.navCtrl.push(SignupPage);
   }
-  presentToast() {
+  presentToast(message) {
     const toast = this.toastCtrl.create({
-      message: 'Login Sucessful',
-      duration: 1000,
+      message: message,
+      duration: 1,
       position: 'top'
     });
 
